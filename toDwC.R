997dcbc6571b71cfee3655053a7c2737aaa2b33b
vmshirey <vms55@georgetown.edu>
2019-08-03 18:34:32 -0400

    Create toDwC.R

---
+++ toDwC.R
@@ -0,0 +1,16 @@
+# Script for converting a survey by species contingency table into simple DwC 
+# using a site by species matrix and survey description table
+#
+# Author: Vaughn M. Shirey
+
+library(dplyr) # functions require dplyr
+
+toSimpleDwC <- function(surveyBySpecies, surveys){
+  
+  # accepts two data frames (surveyBySpecies, surveys)
+  # converts a survey by species matrix and survey information into a simple DwC conforming dataframe
+  # returns the simple DwC conforming dataframe
+  
+  
+  
+}
