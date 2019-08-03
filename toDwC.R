5b082292775ab4e503f0b7a69fb2746a079a845a
vmshirey <vms55@georgetown.edu>
2019-08-03 19:12:01 -0400

    Update toDwC.R

--- toDwC.R
+++ toDwC.R
@@ -5,14 +5,14 @@
 
 library(dplyr) # functions require dplyr
 
-yukonSpecies <- read.table(text = "SurveyCode Boloria_polaris Boloria_chariclea Parnassius_eversmanni
-                           AA_2019-07-13  1 2 0
-                           AA_2019-08-13  1 1 3
-                           BB_2019-08-13  2 4 4", header = TRUE, stringsAsFactors = FALSE)
-yukonSurveys <- read.table(text = "SurveyCode  eventDate observer
-                           AA_2019-07-13  07/13/2019  Green Team
-                           AA_2019-08-13  08/13/2019  Blue Team
-                           BB_2019-08-13  08/13/2019  Green Team", header = TRUE, stringsAsFactors = FALSE)
+yukonSpecies <- read.table(text = "SurveyCode,Boloria_polaris,Boloria_chariclea,Parnassius_eversmanni
+                           AA_2019-07-13,1,2,0
+                           AA_2019-08-13,1,1,3
+                           BB_2019-08-13,2,4,4", header = TRUE, stringsAsFactors = FALSE, sep = ",")
+yukonSurveys <- read.table(text = "SurveyCode,eventDate,observer
+                           AA_2019-07-13,07/13/2019,Green Team
+                           AA_2019-08-13,08/13/2019,Blue Team
+                           BB_2019-08-13,08/13/2019,Green Team", header = TRUE, stringsAsFactors = FALSE, sep = ",")
 
 toSimpleDwC <- function(surveyBySpecies, surveys, transect = FALSE, transectWidth = 0.0, gpsPoint = FALSE, gpsPointError = 0.0){
   
@@ -27,7 +27,24 @@ toSimpleDwC <- function(surveyBySpecies, surveys, transect = FALSE, transectWidt
   # Returns the simple DwC conforming dataframe
   
   merged <- merge(x = surveyBySpecies, y = surveys, by = "SurveyCode")
+  dOutput <- data.frame()
   
+  print("Creating simple DwC records from matrix data...")
+  
+  for(i in 1:(nrow(surveyBySpecies))){
+    
+    for(j in 2:(ncol(surveyBySpecies)-ncol(surveys))){
+      
+      # If occurrence count is non-zero, append a new occurrence record to the output dataframe
+      if(merged[i,j] != 0){
+        dOutput[nrow(dOutput) + 1,] = c("test", "data")
+      }
+    }
+  }
+  
+  # Set column names using DwC terms
+  
+  return(dOutput)
 }
 
 # Example usage:
